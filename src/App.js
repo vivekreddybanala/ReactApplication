7da6043c6720f545a14d8e04751d7e54c955a183
vbanala <vbanala@atd-us.com>
2021-06-14 20:50:38 -0400

    june 14 changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customers from './Customers'
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import Home from './Home';
 
 
